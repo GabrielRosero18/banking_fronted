9597fbb58e08b95921b23749aa22d303556b7506
Gabuchis <gabrielrosero180106@gmail.com>
2025-10-22 16:34:12 -0500

    banking landing page

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <section className="hero-section">
+        <div className="hero-content">
+          <h1>Welcome to My Bank</h1>
+          
+          <p>Your reliable partner in managing financial with ease and security.</p>
+          <a href="/get-started" className="cta-button">Get Started</a> 
+        </div>
+      </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
 
-export default App
+export default App;
